e72be12aac1c728f94ed16bfa1125308b5461ab6
Matan <Matan@Matan-PC>
2012-12-09 23:47:06 +0200

    hi

--- p2p_sim/src/graph_generators/ChordGenerator.java
+++ p2p_sim/src/graph_generators/ChordGenerator.java
@@ -1,5 +1,46 @@
 package graph_generators;
 
-public class ChordGenerator extends HashRingGenerator {
 
+import java.util.SortedSet;
+import java.util.TreeSet;
+
+import org.jgrapht.graph.DefaultEdge;
+import org.jgrapht.Graph;
+
+public class ChordGenerator extends HashRingGenerator {
+	private static int successorLength=1;
+	public static void setSuccessorLength(int ui)
+	{
+		if(ui<=0)
+		{
+			throw(new IllegalArgumentException());
+		}
+		successorLength = ui;
+	}
+	public static int getSuccessorLength()
+	{
+		return successorLength;
+	}
+	public static Graph<HashRingNode, DefaultEdge> generateChord(int numberOfNodes)
+	{
+		Graph<HashRingNode,DefaultEdge> $ = HashRingGenerator.generateHashRingGraph(numberOfNodes, true);
+		addFingers($);
+		
+		return $;
+	}
+	private static void addFingers(Graph<HashRingNode,DefaultEdge> graph)
+	{
+		SortedSet<HashRingNode> s = new TreeSet<>(graph.vertexSet());
+		for(HashRingNode node: graph.vertexSet())
+		{
+			for(int i = 0 ; i < 64 ; ++i)
+			{
+				double next = (node.getHashKey()+((double)1)/(1<<64-i))%1;
+				HashRingNode successor = s.tailSet(new HashRingNode(next)).first();
+				if(!graph.containsEdge(node, successor))
+					graph.addEdge(node,successor);
+			}
+		}
+	}
+//	private static 
 }
