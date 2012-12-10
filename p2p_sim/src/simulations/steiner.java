5013462673eb9b8088585389bfb3cb6ff96f25bc
Matan <Matan@Matan-PC>
2012-12-10 03:10:37 +0200

    started implementing BFS

--- p2p_sim/src/simulations/steiner.java
+++ p2p_sim/src/simulations/steiner.java
@@ -1,5 +1,36 @@
 package simulations;
+import java.util.HashMap;
+import java.util.Map;
 
+import org.jgrapht.Graph;
+import org.jgrapht.graph.DefaultEdge;
+import org.jgrapht.graph.SimpleGraph;
+
+import graph_generators.*;
 public class steiner {
 
+	public static Graph<HashRingNode,DefaultEdge> steinerApprox(Graph<HashRingNode,DefaultEdge> ring)
+	{
+//		Graph<HashRingNode,DefaultEdge> GC = 
+		return ring;
+	}
+	
+	private Graph<HashRingNode,DefaultEdge> getGC(Graph<HashRingNode,DefaultEdge> ring)
+	{
+		Graph<HashRingNode,DefaultEdge> $ = new SimpleGraph<>(DefaultEdge.class);
+		for(HashRingNode i : ring.vertexSet())
+		{
+			$.addVertex(i);
+		}
+		
+		return $;
+		
+	}
+	
+	private static Map<HashRingNode, Integer> BFS(HashRingNode src, Graph<HashRingNode,DefaultEdge> ring)
+	{
+		Map<HashRingNode,Integer> $ = new HashMap<>(ring.vertexSet().size());
+		
+		return $;
+	}
 }
